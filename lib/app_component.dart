a48fa57a789355a7f9db965c715d7e3dfc0ef414
chikuwa111 <take0shun1111@gmail.com>
2020-12-28 00:24:26 +0900

    Finish 2. Master/Detail

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,22 +1,19 @@
 import 'package:angular/angular.dart';
+import 'package:angular_app/src/hero.dart';
+import 'package:angular_app/src/mock_heroes.dart';
 import 'package:angular_forms/angular_forms.dart';
-
-import 'package:angular_app/hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '''
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}}</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-  ''',
-  directives: [formDirectives],
+  templateUrl: 'app_component.html',
+  styleUrls: ['app_component.css'],
+  directives: [coreDirectives, formDirectives],
 )
 class AppComponent {
   final title = 'Tour of Heroes';
-  Hero hero = Hero(1, 'Windstorm');
+
+  List<Hero> heroes = mockHeroes;
+  Hero selected;
+
+  void onSelect(Hero hero) => selected = hero;
 }
